0090d1ea4f8d924b9cd803fc9a300e8b1778c99b
Roger Santos <106624531+rogimpl@users.noreply.github.com>
2024-03-06 16:49:21 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 let nomeHeroi = "Pedro"
-let xpHeroi = " 1500"
+let xpHeroi = "11000"
 let xp = ""
 
 if(xpHeroi <= 1000){
@@ -10,6 +10,14 @@ if(xpHeroi <= 1000){
   xp= "Prata"
 }else if (xpHeroi <= 7000){
     xp =" Ouro"
+}else if (xpHeroi <= 8000){
+    xp ="Platina"
+}else if (xpHeroi <= 9000){
+    xp = "Ascendente"
+}else if (xpHeroi <= 10000){
+    xp ="Imortal"
+}else if ( xpHeroi >= 10001){
+  xp = "Radiante"
 }
 
 console.log("O Herói de nome",nomeHeroi,"está no nível de",xp)
