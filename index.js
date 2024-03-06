fa60fb27d6e8f9fe6cbe487e39b64e10c67a2c4f
rogimpl <rogimpl@hotmail.com>
2024-03-06 16:29:37 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+let nomeHeroi = "Pedro"
+let xpHeroi = " 1500"
+let xp = ""
+
+if(xpHeroi <= 1000){
+   xp = "Ferro"
+}else if (xpHeroi <= 2000){
+  xp = "Bronze"
+}else if (xpHeroi <= 5000){
+  xp= "Prata"
+}else if (xpHeroi <= 7000){
+    xp =" Ouro"
+}
+
+console.log("O Herói de nome",nomeHeroi,"está no nível de",xp)
+
+
+
